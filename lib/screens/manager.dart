7972e873abf24dcbbad66e1de1cbb33ce8894333
Branislav <branislavprokop98@gmail.com>
2021-10-21 10:19:30 +0200

    implements main page for routing

--- lib/screens/manager.dart
+++ lib/screens/manager.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tp_mobile_app/routes/router.gr.dart';
 
 class MainPage extends StatelessWidget {
   MainPage({Key? key}) : super(key: key);
 
+  final _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold()
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Bottom Nav Bar with Nested Routing',
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
+    );
   }
 }
