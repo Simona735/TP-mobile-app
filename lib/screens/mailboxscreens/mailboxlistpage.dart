d3d450591b9f5f0135629b66309ecf33460866d4
Branislav <branislavprokop98@gmail.com>
2021-10-21 10:12:23 +0200

    created appbar mailbox list page

--- lib/screens/mailboxscreens/mailboxlistpage.dart
+++ lib/screens/mailboxscreens/mailboxlistpage.dart
@@ -6,6 +6,9 @@ class ListOfMailboxes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Zoznam schránok"),
+      ),
     );
   }
 }
