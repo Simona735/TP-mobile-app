30778e3389a95a2d79bd2728d82c4b9f50de5447
Branislav <branislavprokop98@gmail.com>
2021-10-21 10:14:00 +0200

    created appbar settings page

--- lib/screens/mailboxscreens/settingspage.dart
+++ lib/screens/mailboxscreens/settingspage.dart
@@ -6,6 +6,9 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Nastavenia"),
+      ),
     );
   }
 }
