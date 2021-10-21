8f785d9b069f5774ea93326e24848c72e3f8a2f1
Branislav <branislavprokop98@gmail.com>
2021-10-21 10:08:24 +0200

    added forms for login

--- lib/screens/authscreens/loginpage.dart
+++ lib/screens/authscreens/loginpage.dart
@@ -16,6 +16,54 @@ class LoginPage extends StatelessWidget {
       resizeToAvoidBottomInset: true,
       body: ListView(
         children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(top: 80),
+            color: Colors.blue,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'E-mail'),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Heslo'),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
+            child: ElevatedButton(
+                onPressed: () {
+                },
+                child: const Text("Prihlásenie")
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
+            child: ElevatedButton(
+              onPressed: () {
+              },
+              child: const Text("Registrácia"),
+              style: ElevatedButton.styleFrom(primary: Colors.red),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(15),
+            child: IconButton(
+              color: Colors.blue,
+              onPressed: () {},
+              iconSize: 60,
+              icon: const Icon(
+                Icons.fingerprint_outlined,
+                size: 60,
+              ),
+            ),
+          ),
         ],
       ),
     );
