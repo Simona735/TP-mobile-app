7482629f9dfcdcceb4f69968fc51ff1c91927906
Branislav <branislavprokop98@gmail.com>
2021-10-21 10:09:51 +0200

    added forms for registration

--- lib/screens/authscreens/registrationpage.dart
+++ lib/screens/authscreens/registrationpage.dart
@@ -11,7 +11,64 @@ class RegistrationPage extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-
+          Container(
+            margin: const EdgeInsets.only(top: 80),
+            color: Colors.blue,
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Meno'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Priezvisko'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'E-mail'),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Heslo'),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Opakovanie hesla'),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text("Registrácia"),
+              style: ElevatedButton.styleFrom(primary: Colors.red),
+            ),
+          ),
         ],
       ),
     );
