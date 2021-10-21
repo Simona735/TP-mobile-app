42c8caf9feab42a8fdc0ea73d13f4d64b8310186
Branislav <branislavprokop98@gmail.com>
2021-10-21 10:15:28 +0200

    created routes for screens

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,9 +1,51 @@
 import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:tp_mobile_app/screens/mailboxscreens/addmailbox.dart';
+import 'package:tp_mobile_app/screens/mailboxscreens/mailboxlistpage.dart';
+import 'package:tp_mobile_app/screens/mailboxscreens/profilepage.dart';
+import 'package:tp_mobile_app/screens/mailboxscreens/settingspage.dart';
+import 'package:tp_mobile_app/widgets/bottombar.dart';
 
 @MaterialAutoRouter(
   routes: <AutoRoute>[
-
+    AutoRoute(
+      path: '/',
+      page: BottomBar,
+      children: [
+        AutoRoute(
+          path: 'mailbox',
+          name: 'MailboxRouter',
+          page: EmptyRouterPage,
+          children: [
+            AutoRoute(path: '', page: ListOfMailboxes),
+          ],
+        ),
+        AutoRoute(
+          path: 'settings',
+          name: 'SettingsRouter',
+          page: EmptyRouterPage,
+          children: [
+            AutoRoute(path: '', page: SettingsPage),
+          ],
+        ),
+        AutoRoute(
+          path: 'addmailbox',
+          name: 'AddMailboxRouter',
+          page: EmptyRouterPage,
+          children: [
+            AutoRoute(path: '', page: AddMailbox),
+          ],
+        ),
+        AutoRoute(
+          path: 'profilepage',
+          name: 'ProfilePageRouter',
+          page: EmptyRouterPage,
+          children: [
+            AutoRoute(path: '', page: ProfilePage),
+          ],
+        ),
+      ],
+    ),
   ],
 )
 class $AppRouter {}
