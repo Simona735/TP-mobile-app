86069b470b43af52db973276288d5f312d6bb9f8
Branislav <branislavprokop98@gmail.com>
2021-10-21 10:10:29 +0200

    created mailbox page

---
+++ lib/screens/mailboxscreens/addmailbox.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AddMailbox extends StatelessWidget {
+  const AddMailbox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
