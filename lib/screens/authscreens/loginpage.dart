4e12f2086c717a8060e338a5786e0f239f7be96d
Branislav <branislavprokop98@gmail.com>
2021-10-21 10:07:22 +0200

    added appbar and listview

--- lib/screens/authscreens/loginpage.dart
+++ lib/screens/authscreens/loginpage.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:tp_mobile_app/screens/authscreens/registrationpage.dart';
+import 'package:tp_mobile_app/widgets/animations.dart';
+
+import '../manager.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      resizeToAvoidBottomInset: true,
+      body: ListView(
+        children: <Widget>[
+        ],
+      ),
+    );
   }
 }
