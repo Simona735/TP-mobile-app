654d28fecc6a269fb8beb1a34ffbe6d7637c3604
Simona735 <richterova735@gmail.com>
2021-11-01 19:04:59 +0100

    auth methods core added

--- lib/firebase/authentication.dart
+++ lib/firebase/authentication.dart
@@ -16,23 +16,60 @@ class Authentication {
 
   Future<void> init() async {
     await Firebase.initializeApp();
+    FirebaseAuth.instance.userChanges().listen((User? user) {
+      if (user == null) {
+        loginState = ApplicationLoginState.loggedOut;
+      } else {
+        loginState = ApplicationLoginState.loggedIn;
+      }
+    });
   }
 
   void verifyEmail(String email) async {
+    var methods = await FirebaseAuth.instance.fetchSignInMethodsForEmail(email);
+    if (methods.contains('password')) {
+      // user and signing method exists
 
+    }
+    this.email = email;
   }
 
   void signInWithEmailAndPassword(String email, String password) async {
-
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
   }
 
   void registerAccount(String name, String surname, String email, String password) async {
+    try {
+      var credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email,
+          password: password
+      ); // user will be signed in, userChange() listener will be activated
+      //TODO write name and surname to DB
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
 
   }
 
   void signOut() {
     FirebaseAuth.instance.signOut();
   }
-
 
 }
