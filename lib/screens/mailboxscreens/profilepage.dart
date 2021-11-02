cc6349fb4c6097f72d8958938ab435510067e9a0
Branislav <branislavprokop98@gmail.com>
2021-11-02 13:37:04 +0100

    fixed overflow issue

--- lib/screens/mailboxscreens/profilepage.dart
+++ lib/screens/mailboxscreens/profilepage.dart
@@ -6,50 +6,51 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: ListView(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Icon(
-                Icons.account_circle,
-                size: 200,
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                "Meno",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 32
-                ),
-              ),
-            ],
-          ),
           Column(
             children: [
-              Container(
-                margin: const EdgeInsets.only(left: 5, right: 5),
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {  },
-                  child: const Text("Zmeniť heslo"),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.account_circle,
+                    size: 200,
+                  ),
+                ],
               ),
-              Container(
-                margin: const EdgeInsets.only(left: 5, right: 5),
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {  },
-                  child: const Text("Odhlásiť sa"),
-                  style: ElevatedButton.styleFrom(primary: Colors.red),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Meno",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 5, right: 5),
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Zmeniť heslo"),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 5, right: 5),
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Odhlásiť sa"),
+                      style: ElevatedButton.styleFrom(primary: Colors.red),
+                    ),
+                  )
+                ],
               )
             ],
-          )
+          ),
         ],
       ),
     );
