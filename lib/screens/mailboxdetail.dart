2b68f7a4724c6da4472f11156a1aa8580bd02fe9
Branislav <branislavprokop98@gmail.com>
2021-11-11 22:00:51 +0100

    created widget for mailbox detail

---
+++ lib/screens/mailboxdetail.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MailboxDetail extends StatefulWidget {
+  const MailboxDetail({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MailboxDetail> createState() => _MailboxDetailState();
+}
+
+class _MailboxDetailState extends State<MailboxDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+    );
+  }
+}
