2193bb5318fdff93c70e610cf4edafb961254090
Branislav <branislavprokop98@gmail.com>
2021-11-11 22:01:43 +0100

    added path param for mailbox id

--- lib/screens/mailboxdetail.dart
+++ lib/screens/mailboxdetail.dart
@@ -1,8 +1,12 @@
+import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 
 class MailboxDetail extends StatefulWidget {
+  final mailboxId;
+
   const MailboxDetail({
     Key? key,
+    @PathParam() required this.mailboxId,
   }) : super(key: key);
 
   @override
