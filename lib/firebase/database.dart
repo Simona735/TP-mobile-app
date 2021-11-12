ddd57df5ed31ba9c69cf9fe54f0dca1dacad74c5
EditaVcelkovaBP <72685743+EditaVcelkovaBP@users.noreply.github.com>
2021-11-12 11:21:23 +0100

    Firebase reference and create record to db

--- lib/firebase/database.dart
+++ lib/firebase/database.dart
@@ -1,7 +1,12 @@
 
 class Database{
-  FirebaseFirestore db = FirebaseFirestore.getInstance();
+  FirebaseFirestore db = FirebaseFirestore.instance.reference(); // Instance on DB
 
+  void createRecord(){
+    db.child( "1" ).set({ // Create record with "id":1
+      'title' : 'test' // The content of the title record is a test
+    });
+  }
 
 
 }
