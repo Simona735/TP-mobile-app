e572723e8268541079c4fd123692df7787dd45b6
EditaVcelkovaBP <72685743+EditaVcelkovaBP@users.noreply.github.com>
2021-11-12 11:29:08 +0100

    Create methods for communication with the firebase database

--- lib/firebase/database.dart
+++ lib/firebase/database.dart
@@ -1,12 +1,26 @@
 
 class Database{
-  FirebaseFirestore db = FirebaseFirestore.instance.reference(); // Instance on DB
+  final FirebaseFirestore db = FirebaseFirestore.instance.reference(); // Instance on DB
 
   void createRecord(){
     db.child( "1" ).set({ // Create record with "id":1
       'title' : 'test' // The content of the title record is a test
     });
   }
+  void getData(){
+    db.once().then((Snapshot DataSnapshot) {
+      print( 'Data: ${snapshot. value } ' ); // Print all record from DB
+    });
+  }
 
+  void updateData(){
+    databaseReference.child('1').update({
+      'description': 'test update' // Update
+    });
+  }
+
+  void deleteData(){
+    databaseReference.child('1').remove(); //Delete
+  }
 
 }
