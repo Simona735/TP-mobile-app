c8799b7e257aca967531f48f8679d00b59575117
Branislav <branislavprokop98@gmail.com>
2021-11-14 17:47:40 +0100

    created initState with sign in check

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:tp_mobile_app/firebase/authentication.dart';
+import 'package:tp_mobile_app/routes/router.gr.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -8,6 +11,19 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      if (Authentication.isSignedIn) {
+        Navigator.of(context).pop();
+        AutoRouter.of(context).push(BottomBarRoute());
+      } else {
+        Navigator.of(context).pop();
+        AutoRouter.of(context).push(const LoginPageRoute());
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
