25dd4f1940d887524d7e4ef8faa83c4ea683e2ca
Branislav <branislavprokop98@gmail.com>
2021-11-14 17:44:31 +0100

    added route for splash screen

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,16 +1,23 @@
 import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:tp_mobile_app/screens/authscreens/loginpage.dart';
+import 'package:tp_mobile_app/screens/authscreens/registrationpage.dart';
 import 'package:tp_mobile_app/screens/mailboxdetail.dart';
 import 'package:tp_mobile_app/screens/mailboxscreens/addmailbox.dart';
 import 'package:tp_mobile_app/screens/mailboxscreens/mailboxlistpage.dart';
 import 'package:tp_mobile_app/screens/mailboxscreens/profilepage.dart';
 import 'package:tp_mobile_app/screens/mailboxscreens/settingspage.dart';
+import 'package:tp_mobile_app/screens/splash_screen.dart';
 import 'package:tp_mobile_app/widgets/bottombar.dart';
 
 @MaterialAutoRouter(
   routes: <AutoRoute>[
     AutoRoute(
       path: '/',
+      page: SplashScreen
+    ),
+    AutoRoute(
+      path: '/bottomBar',
       page: BottomBar,
       children: [
         AutoRoute(
