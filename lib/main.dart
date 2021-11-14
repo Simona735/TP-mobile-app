85847e906bcef804721fce4ab3e48948095b9caa
Branislav <branislavprokop98@gmail.com>
2021-11-14 17:51:45 +0100

    edited application start point

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:tp_mobile_app/firebase/authentication.dart';
-import 'package:tp_mobile_app/screens/authscreens/loginpage.dart';
-import 'package:firebase_core/firebase_core.dart'; // new
+import 'package:firebase_core/firebase_core.dart';
+import 'package:tp_mobile_app/screens/manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Authentication.init();
-  runApp(const MyApp());
+  runApp(MainPage());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginPage(),
-    );
-  }
-}
-
 
