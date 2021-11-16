5f1a4470b082c4acf87d6142340a38a1f041cdd9
Branislav <branislavprokop98@gmail.com>
2021-11-16 14:28:35 +0100

    added information screen about bluetooth state

--- lib/screens/bluetooth/bluetooth_off.dart
+++ lib/screens/bluetooth/bluetooth_off.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 class BluetoothOffScreen extends StatelessWidget {
-  const BluetoothOffScreen({Key? key}) : super(key: key);
+  const BluetoothOffScreen({Key? key, this.state}) : super(key: key);
+
+  final BluetoothState? state;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Icon(
+              Icons.bluetooth_disabled,
+              size: 200.0,
+              color: Colors.white54,
+            ),
+            Text(
+              'Bluetooth Adapter je ${state != null ? 'vypnutý' : 'nedostupny'}.',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
