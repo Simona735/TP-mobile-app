ec6fcbd0bfffb8e7f616c75baef18952a725ba24
Branislav <branislavprokop98@gmail.com>
2021-11-18 20:50:55 +0100

    implementation of application login and launch logic

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,10 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tp_mobile_app/firebase/authentication.dart';
 import 'package:tp_mobile_app/routes/router.gr.dart';
+import 'dart:developer' as developer;
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -17,13 +20,29 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
-      if (Authentication.isSignedIn) {
-        Navigator.of(context).pop();
-        AutoRouter.of(context).push(BottomBarRoute());
+    WidgetsBinding.instance!.addPostFrameCallback((_) async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? value = prefs.getString('pin');
+      if (value != null) {
+        developer.log(value);
+        if (Authentication.isSignedIn && value.isNotEmpty) {
+          Navigator.of(context).pop();
+          AutoRouter.of(context).push(const PinPageRoute());
+        } else if (Authentication.isSignedIn && value.isEmpty) {
+          Navigator.of(context).pop();
+          AutoRouter.of(context).push(BottomBarRoute());
+        } else {
+          Navigator.of(context).pop();
+          AutoRouter.of(context).push(const LoginPageRoute());
+        }
       } else {
-        Navigator.of(context).pop();
-        AutoRouter.of(context).push(const LoginPageRoute());
+        if (Authentication.isSignedIn) {
+          Navigator.of(context).pop();
+          AutoRouter.of(context).push(BottomBarRoute());
+        } else {
+          Navigator.of(context).pop();
+          AutoRouter.of(context).push(const LoginPageRoute());
+        }
       }
     });
   }
