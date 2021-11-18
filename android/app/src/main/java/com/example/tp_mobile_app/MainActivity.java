78d515775191dc6ef73ebc65976ce477e91bc6d9
Branislav <branislavprokop98@gmail.com>
2021-11-18 20:21:46 +0100

    implementing biometric service

--- android/app/src/main/java/com/example/tp_mobile_app/MainActivity.java
+++ android/app/src/main/java/com/example/tp_mobile_app/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.tp_mobile_app;
 
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.embedding.android.FlutterActivity;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterFragmentActivity {
 }
