270d265983dfbfe04adb81ee5d0a11b57fc13a20
Branislav <branislavprokop98@gmail.com>
2021-11-18 20:47:30 +0100

    reformatted code

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -28,8 +28,8 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.white,
-        child:FlutterLogo(size:MediaQuery.of(context).size.height)
+      color: Colors.white,
+      child: FlutterLogo(size: MediaQuery.of(context).size.height),
     );
   }
 }
