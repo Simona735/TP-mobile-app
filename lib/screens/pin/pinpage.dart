0d2420e0f02ca460d25ee1f073c51bcdaf6e21fd
Branislav <branislavprokop98@gmail.com>
2021-11-18 20:38:49 +0100

    created pin page

---
+++ lib/screens/pin/pinpage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PinPage extends StatelessWidget {
+  const PinPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
