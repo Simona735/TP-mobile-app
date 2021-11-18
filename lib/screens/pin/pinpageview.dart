3198e97e1ce4a5b2c58c35ed3ee05a0cff3e7c2f
Branislav <branislavprokop98@gmail.com>
2021-11-18 20:34:41 +0100

    created pin page view

---
+++ lib/screens/pin/pinpageview.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PinPutView extends StatefulWidget {
+  const PinPutView({Key? key}) : super(key: key);
+
+  @override
+  PinPutViewState createState() => PinPutViewState();
+}
+
+class PinPutViewState extends State<PinPutView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
