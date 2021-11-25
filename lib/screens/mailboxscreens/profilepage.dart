70cd75c04815fd7408e1b558cba1fc1ecc1eebe8
Simona735 <richterova735@gmail.com>
2021-11-25 15:23:51 +0100

    removed unused imports

--- lib/screens/mailboxscreens/profilepage.dart
+++ lib/screens/mailboxscreens/profilepage.dart
@@ -2,9 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:tp_mobile_app/firebase/authentication.dart';
 import 'package:tp_mobile_app/routes/router.gr.dart';
-import 'package:tp_mobile_app/screens/authscreens/loginpage.dart';
-
-import '../passwordchange.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
