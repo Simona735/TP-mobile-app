5f5b0281f6152c122a381e6f347aeec96aa5300d
Branislav <branislavprokop98@gmail.com>
2021-12-01 15:56:45 +0100

    added appbar in mailbox detail page

--- lib/screens/mailboxdetail.dart
+++ lib/screens/mailboxdetail.dart
@@ -23,82 +23,87 @@ class _MailboxDetailState extends State<MailboxDetail> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: CircularStepProgressIndicator(
-                totalSteps: limit.round(),
-                currentStep: listCount,
-                stepSize: 5,
-                selectedColor:
-                    listCount <= limit.round() ? Colors.yellow : Colors.red,
-                unselectedColor: Colors.grey[200],
-                padding: 0,
-                width: 150,
-                height: 150,
-                selectedStepSize: 15,
-                roundedCap: (_, __) => true,
-                child: Center(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      listCount.toString(),
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 25),
-                    ),
-                  ],
-                )),
-              ),
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            Slider(
-              value: limit,
-              onChanged: (value) {
-                setState(() {
-                  limit = value;
-                });
-              },
-              min: 1.0,
-              max: 10.0,
-              activeColor: Colors.yellow,
-              inactiveColor: Colors.yellow[100],
-              label: limit.round().toString(),
-              divisions: 9,
-            ),
-            Text(
-              "Limit: " + limit.round().toString(),
-              style: const TextStyle(fontSize: 20),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(left: 10),
-                  child: const Text("Low power mode"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Detail schránky"),
+      ),
+      body: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: CircularStepProgressIndicator(
+                  totalSteps: limit.round(),
+                  currentStep: listCount,
+                  stepSize: 5,
+                  selectedColor:
+                      listCount <= limit.round() ? Colors.yellow : Colors.red,
+                  unselectedColor: Colors.grey[200],
+                  padding: 0,
+                  width: 150,
+                  height: 150,
+                  selectedStepSize: 15,
+                  roundedCap: (_, __) => true,
+                  child: Center(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        listCount.toString(),
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 25),
+                      ),
+                    ],
+                  )),
                 ),
-                Switch(
-                  value: isSwitched,
-                  onChanged: (value) => {
-                    setState(
-                      () {
-                        isSwitched = value;
-                      },
-                    )
-                  },
-                )
-              ],
-            )
-          ],
-        )
-      ],
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Slider(
+                value: limit,
+                onChanged: (value) {
+                  setState(() {
+                    limit = value;
+                  });
+                },
+                min: 1.0,
+                max: 10.0,
+                activeColor: Colors.yellow,
+                inactiveColor: Colors.yellow[100],
+                label: limit.round().toString(),
+                divisions: 9,
+              ),
+              Text(
+                "Limit: " + limit.round().toString(),
+                style: const TextStyle(fontSize: 20),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 10),
+                    child: const Text("Low power mode"),
+                  ),
+                  Switch(
+                    value: isSwitched,
+                    onChanged: (value) => {
+                      setState(
+                        () {
+                          isSwitched = value;
+                        },
+                      )
+                    },
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
