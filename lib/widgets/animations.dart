d0d1394cd5bfd4042541291472f8aa7267105203
Branislav <branislavprokop98@gmail.com>
2021-12-01 15:51:59 +0100

    implementation of route animation

--- lib/widgets/animations.dart
+++ lib/widgets/animations.dart
@@ -23,3 +23,22 @@ class FadeRoute extends PageRouteBuilder {
           ),
         );
 }
+
+Route swipeRouteAnimation(Widget page) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => page,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(-1.0, 0.0);
+      const end = Offset.zero;
+      const curve = Curves.easeInCubic;
+
+      var tween =
+      Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
