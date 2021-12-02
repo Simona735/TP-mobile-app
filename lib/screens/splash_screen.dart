ad60b3a2feec5ee7a129b2d8440551d58c5d22a5
Branislav <branislavprokop98@gmail.com>
2021-12-02 17:48:04 +0100

    setting the theme mode at application startup

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tp_mobile_app/firebase/authentication.dart';
 import 'package:tp_mobile_app/routes/router.gr.dart';
@@ -23,6 +25,13 @@ class _SplashScreenState extends State<SplashScreen> {
     WidgetsBinding.instance!.addPostFrameCallback((_) async {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       String? value = prefs.getString('pin');
+      bool valueDark = prefs.getBool('darkTheme') ?? false;
+      if (valueDark) {
+        Get.changeThemeMode(ThemeMode.dark);
+      } else {
+        Get.changeThemeMode(ThemeMode.light);
+      }
+
       if (value != null) {
         developer.log(value);
         if (Authentication.isSignedIn && value.isNotEmpty) {
