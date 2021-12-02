ebb3a7b2614b85eeee18fcc90adc7a62bfe0e665
Branislav <branislavprokop98@gmail.com>
2021-12-02 17:41:20 +0100

    added themes for light and dark theme

--- lib/screens/manager.dart
+++ lib/screens/manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:tp_mobile_app/routes/router.gr.dart';
+import 'package:tp_mobile_app/widgets/customtheme.dart';
 
 class MainPage extends StatelessWidget {
   MainPage({Key? key}) : super(key: key);
@@ -12,6 +13,9 @@ class MainPage extends StatelessWidget {
     return GetMaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Mailbox',
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      themeMode: ThemeMode.system,
       routerDelegate: _appRouter.delegate(),
       routeInformationParser: _appRouter.defaultRouteParser(),
     );
