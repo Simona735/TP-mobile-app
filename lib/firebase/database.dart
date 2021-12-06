9402757ee58099894eb2ff3792487fe756402910
EditaVcelkovaBP <72685743+EditaVcelkovaBP@users.noreply.github.com>
2021-12-06 16:54:37 +0100

    Creating functions to work with data from the service table

--- lib/firebase/database.dart
+++ lib/firebase/database.dart
@@ -4,24 +4,42 @@ class Database{
   static final DatabaseReference _messagesRef =
   FirebaseDatabase.instance.reference();// Instance on DB
 
-  static void createRecord(){
-    _messagesRef.child( "1" ).set({ // Create record with "id":1
-      'title' : 'test' // The content of the title record is a test
-    });
-  }
-  static void getData(){
-    _messagesRef.once().then((DataSnapshot snapshot) {
-      print( 'Data: ${snapshot. value } ' ); // Print all record from DB
-    });
-  }
-  static void updateData(){
-    _messagesRef.child('1').update({
-      'description': 'test update' // Update
+
+  // Create record  DATABASE with child "user_id": user_id ,"mailbox_id":mailbox_id
+  static void createRecordService(String user_id, String mailbox_id, String counter, String distance){
+    _messagesRef.child('DATABASE/'+user_id +'/'+mailbox_id+'/service/').set({
+      // service -> "counter": counter
+      'counter': counter,
+      // service -> "distance_from_senzor: distance"
+      'distance_from_senzor': distance,
     });
   }
 
-   static void deleteData(){
-    _messagesRef.child('1').remove(); //Delete
+  //Writing data and returning a string with data from the service table
+  static String getDataService(String user_id, String mailbox_id){
+    String data = '';
+    //Redirect to service table
+    _messagesRef.child('DATABASE/'+user_id +'/'+mailbox_id+'/service/').once().then((DataSnapshot snapshot) {
+      // Print all data from the service table
+      print( 'Data SERVICE: ${snapshot. value } ' );
+      data = snapshot.value.toString();
+    });
+    return data;
+  }
+
+  // To change the data for a user's mailbox service table
+  static void updateDataService(String user_id, String mailbox_id, String counter, String distance){
+    //Redirect to service table and update
+    _messagesRef.child('DATABASE').child(user_id).child(mailbox_id).child('service').update({
+      'counter': counter,
+      'distance_from_senzor': distance
+    });
+  }
+
+  //Removing data from the service table for a user's mailbox
+   static void deleteDataService(String user_id, String mailbox_id){
+    //Redirect to service table an remove
+    _messagesRef..child('DATABASE').child(user_id).child(mailbox_id).child('service').remove();
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tp_mobile_app/firebase/authentication.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:tp_mobile_app/firebase/database.dart';
 import 'package:tp_mobile_app/screens/manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Authentication.init();
+  Database.getDataService('user_id', 'mailbox_id');
   runApp(MainPage());
 }
 
