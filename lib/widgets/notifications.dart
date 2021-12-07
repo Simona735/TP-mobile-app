a72630d3d90e4801d7cb319ef6628b2486f56ed8
Branislav <branislavprokop98@gmail.com>
2021-12-07 15:03:40 +0100

    added SnackBar notification

--- lib/widgets/notifications.dart
+++ lib/widgets/notifications.dart
@@ -1,3 +1,26 @@
-class Notifications {
+import 'package:flutter/material.dart';
 
+class Notifications {
+  static SnackBar snackBar(IconData icon, String text) {
+    return SnackBar(
+      content: SizedBox(
+        height: 40,
+        child: Row(
+          children: [
+            Icon(icon),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text(text),
+            ),
+          ],
+        ),
+      ),
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: const Color.fromRGBO(33, 150, 243, 0.5),
+      elevation: 0.1,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+  }
 }
