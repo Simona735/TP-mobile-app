88a6b794c54bdce629baf43b83c875f7aa7fdcf0
Simona735 <richterova735@gmail.com>
2021-12-08 14:34:49 +0100

    pub get

--- lib/widgets/animations.dart
+++ lib/widgets/animations.dart
@@ -42,3 +42,22 @@ Route swipeRouteAnimation(Widget page) {
     },
   );
 }
+
+Route swipeRouteAnimation(Widget page) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => page,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(-1.0, 0.0);
+      const end = Offset.zero;
+      const curve = Curves.easeInCubic;
+
+      var tween =
+      Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
