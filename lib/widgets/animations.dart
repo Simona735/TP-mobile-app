0ae842f579355f87a6c59dc2923cf1cdbf594d66
Simona735 <richterova735@gmail.com>
2021-12-08 14:36:15 +0100

    removed duplicities in animations

--- lib/widgets/animations.dart
+++ lib/widgets/animations.dart
@@ -42,22 +42,3 @@ Route swipeRouteAnimation(Widget page) {
     },
   );
 }
-
-Route swipeRouteAnimation(Widget page) {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => page,
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      const begin = Offset(-1.0, 0.0);
-      const end = Offset.zero;
-      const curve = Curves.easeInCubic;
-
-      var tween =
-      Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-      return SlideTransition(
-        position: animation.drive(tween),
-        child: child,
-      );
-    },
-  );
-}
