5f411a62060a1532eff5e41907cd1cd4125df94c
Branislav <branislavprokop98@gmail.com>
2022-01-23 12:36:38 +0100

    fixed pin page for tablet

--- lib/screens/pin/pinpage.dart
+++ lib/screens/pin/pinpage.dart
@@ -12,27 +12,27 @@ class PinPage extends StatelessWidget {
         const app = Scaffold(
           body: PinPutView()
         );
-        if (constraints.maxWidth > 600 && constraints.maxHeight > 600) {
-          return Container(
-            color: Colors.white,
-            alignment: Alignment.center,
-            child: AspectRatio(
-              aspectRatio: .5,
-              child: Container(
-                margin: EdgeInsets.all(20),
-                clipBehavior: Clip.antiAlias,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 20,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: app,
-              ),
-            ),
-          );
-        }
+        // if (constraints.maxWidth > 600 && constraints.maxHeight > 600) {
+        //   return Container(
+        //     color: Colors.white,
+        //     alignment: Alignment.center,
+        //     child: AspectRatio(
+        //       aspectRatio: .5,
+        //       child: Container(
+        //         margin: EdgeInsets.all(20),
+        //         clipBehavior: Clip.antiAlias,
+        //         decoration: BoxDecoration(
+        //           border: Border.all(
+        //             color: Colors.black,
+        //             width: 20,
+        //           ),
+        //           borderRadius: BorderRadius.circular(10),
+        //         ),
+        //         child: app,
+        //       ),
+        //     ),
+        //   );
+        // }
         return app;
       },
     );
