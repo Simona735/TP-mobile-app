e49d6658d2cc183768973264f7b1837764774ce2
Branislav <branislavprokop98@gmail.com>
2022-01-23 11:50:55 +0100

    created text editing controllers and their disposal

--- lib/controllers/passwordchange_controller.dart
+++ lib/controllers/passwordchange_controller.dart
@@ -1,5 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class PasswordChangeController extends GetxController{
 
+  final oldPasswordController = TextEditingController();
+  final newPassword1Controller = TextEditingController();
+  final newPassword2Controller = TextEditingController();
+
+  @override
+  void onClose() {
+    oldPasswordController.dispose();
+    newPassword1Controller.dispose();
+    newPassword2Controller.dispose();
+    super.onClose();
+  }
 }
