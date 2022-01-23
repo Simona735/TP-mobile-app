c08be75c36a752e0dc294fc3a95d52ea04844293
Branislav <branislavprokop98@gmail.com>
2022-01-23 12:52:32 +0100

    business logic separated

--- lib/controllers/bottom_bar_controller.dart
+++ lib/controllers/bottom_bar_controller.dart
@@ -1,5 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class BottomBarController extends GetxController{
+  final index = 0.obs;
+  late PageController pageController;
 
+  void updateIndex(int index){
+    this.index.value = index;
+  }
+
+  @override
+  void onInit() {
+    pageController = PageController();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    pageController.dispose();
+    super.onClose();
+  }
 }
