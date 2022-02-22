73d21293095c363f717dd0ba855eb24b6c03b00a
Branislav <branislavprokop98@gmail.com>
2022-02-22 19:04:21 +0100

    parameters for show password added

--- lib/controllers/passwordchange_controller.dart
+++ lib/controllers/passwordchange_controller.dart
@@ -7,6 +7,18 @@ class PasswordChangeController extends GetxController{
   final newPassword1Controller = TextEditingController();
   final newPassword2Controller = TextEditingController();
 
+  final showOldPassword = false.obs;
+  final showPassword1Password = false.obs;
+  final showPassword2Password = false.obs;
+
+  @override
+  void onInit() {
+    showOldPassword.value = false;
+    showPassword1Password.value = false;
+    showPassword2Password.value = false;
+    super.onInit();
+  }
+
   @override
   void onClose() {
     oldPasswordController.dispose();
